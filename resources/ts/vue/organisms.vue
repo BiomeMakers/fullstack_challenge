7f2210fe747db0d6a5f3c7116ff72f607112aea1
Your Name <you@example.com>
2022-05-18 16:32:18 +0200

    Challenges Finished

--- resources/ts/vue/organisms.vue
+++ resources/ts/vue/organisms.vue
@@ -5,8 +5,20 @@
 
        <h2> Organism list ({{ organisms.length }}) </h2>
 
-        <p class="todo">TODO: Complete VUE code</p>
-
+        <table class="pure-table pure-table-horizontal">
+            <thead>
+                <tr>
+                    <th>Genus</th>
+                    <th>Species</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="organism of organisms" :key="organism.id">
+                    <td>{{ organism.genus }}</td>
+                    <td>{{ organism.species }}</td>
+                </tr>
+            </tbody>
+        </table>
    </div>
 
 
@@ -24,11 +36,33 @@
 import {Vue, Component, Prop, Watch} from 'vue-property-decorator';
 import axios from 'axios';
 
+/**
+ * Type of the organism
+ */
+type OrganismType = {
+    genus: string;
+    species: string;
+}
 
 @Component({})
 export default class OrganismsVue extends Vue {
 
-    organisms = [];
+    organisms: OrganismType[] = [];
+    mounted() {
+        this.loadOrganisms();
+    }
 
+    /*
+    * Retrieve the organism list from the api
+    */
+    async loadOrganisms() {
+        var organismsRequest = (await axios.get('/api/organisms')).data;
+
+        while (organismsRequest.next_page_url) {
+            this.organisms.push(...organismsRequest.data)
+            organismsRequest = (await axios.get(organismsRequest.next_page_url)).data;
+        }
+        this.organisms.push(...organismsRequest.data)
+    }
 }
 </script>
